34b05c00beec368dc83332897d26dcd5bb92e87a
Shivesh Pandey <53203340+pshivesh8@users.noreply.github.com>
2022-02-17 21:31:27 +0530

    Create Maximum amount you went over the speed limit.cpp

---
+++ Maximum amount you went over the speed limit.cpp
@@ -0,0 +1,167 @@
+Problem:
+
+There is a road of length 100 km which you travelled. There are different speed limits for different segments of the road. There are N such segments. And if one were to describe your driving, you drove in M segments. In each segment, you maintained a constant speed. You want to find out the maximum amount you went over the speed limit.
+
+Input
+Two integers N M on the first line.
+
+N lines follow, where the i-th line of these describes the i-th segment. Each line has two integers, the first one describes the length of the segment and the second describes the maximum speed limit of this segment.
+
+M lines follow, where the j-th line of these describes the j-th segment. Each line has two integers, the first one describes the length of the segment and the second describes the speed at which you travelled in this segment.
+
+Constraints
+The lengths of all N segements add up to 100.
+
+The lengths of all M segments add up to 100.
+
+All lengths are positive integers.
+
+The speed limit of any segment is between 1 and 100 inclusive.
+
+Your speed in any segment is between 1 and 100 inclusive.
+
+Output
+One integer --- the maximum amount you went over the speed limit. If you never went over the speed limit, output 0.
+
+Example 1
+Input
+3 3
+40 75
+50 35
+10 45
+40 76
+20 30
+40 40
+Output
+5
+
+Limits
+Time: 1s
+Memory: 256 MB
+
+
+Solution:
+
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀
+⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀
+⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀
+⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀
+⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀⠀
+              pshivesh8⠀⠀
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define pii pair<int,int>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+ll solve(vector<pair<ll,ll>>a,vector<pair<ll,ll>>b,ll n,ll m)
+{
+   ll aa[101],bb[101];
+   ll j=1;
+   ll ans=0;
+   for(ll i=1;i<n;i++)
+   a[i].first+=a[i-1].first;
+   for(ll i=0;i<n;i++)
+   {
+       while(j<=a[i].first)
+       {
+           aa[j]=a[i].second;
+           j++;
+
+       }
+   }
+   j=1;
+   for(ll i=1;i<m;i++)
+   b[i].first+=b[i-1].first;
+   for(ll i=0;i<m;i++)
+   {
+       while(j<=b[i].first)
+       {
+           bb[j]=b[i].second;
+           j++;
+       }
+   }
+   for(ll i=1;i<=100;i++)
+   {
+       ans=max(ans,bb[i]-aa[i]);
+   }
+   return ans;
+}
+
+int main()
+{
+    fastio();
+	  ll n, m;
+    cin>>n>>m;
+    vector<pair<ll,ll>>a, b;
+    for(ll i=0; i<n; i++){
+        ll temp1, temp2;
+        cin>>temp1>>temp2;
+        a.push_back({ temp1, temp2 });
+    }
+    for(ll i=0; i<m; i++){
+        ll temp1, temp2;
+        cin>>temp1>>temp2;
+        b.push_back({ temp1, temp2 });
+    }
+
+    cout<<solve(a, b, n, m);
+
+	  return 0;
+}
