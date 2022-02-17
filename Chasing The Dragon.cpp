56e9eff10f331382341da275c9d5cf8e129cbac9
Shivesh Pandey <53203340+pshivesh8@users.noreply.github.com>
2022-02-17 22:10:41 +0530

    Create Chasing The Dragon.cpp

---
+++ Chasing The Dragon.cpp
@@ -0,0 +1,189 @@
+Problem:
+
+There are N cities on the equator forming a ring. The distance between neighbouring cities is 1. That is, when 1<i≤N, the distance between city i−1 and city i is 1 and it would cost you c_i to travel between the two cities. And the distance between city 1 and city N is also 1 while costing you c1.
+
+You are tracking the Sky Dragon and you've figured out its flight pattern. The dragon will perform K attacks. For its i-th attack, it will travel di distance in either the clockwise or anti-clockwise direction and attack whichever city is there. Clockwise indicates: 1→2→⋯−>N→1. Anti-clockwise is the opposite.
+
+Both you and the dragon are currently at city 1 and is preparing to travel to perform its first attack. What is the minimum cost with which you can reach the destination of the dragon's each attack?
+
+Input
+One integer N on the first line.
+
+N integers on the second line. The i-th integer is ci.
+
+One integer K on the third line.
+
+K lines follow. On the j-th of these lines, there are two integers ti and di.
+
+If ti=−1, then the dragon travels di distance in the anti-clockwise direction. If ti=+1, then the dragon travels di distance in the clockwise direction.
+
+Constraints
+1≤N,K≤10^6
+1≤ci≤10^6
+1≤di≤10^18
+
+Output
+K lines with one integer on each line.
+
+On the i-th line, output the minimum cost to travel to the city targeted by the dragon's i-th attack, from the city where the dragon was previously at.
+
+Example 1
+Input
+5
+1 4 5 2 5
+7
+1 1
+1 5
+1 7
+-1 4
+-1 4
+1 8
+-1 2
+Output
+4
+0
+7
+5
+1
+6
+7
+
+Example 2
+Input
+8
+7 4 5 1 5 6 2 7
+13
+1 18
+-1 11
+1 19
+-1 14
+1 17
+-1 13
+-1 4
+-1 8
+-1 11
+1 6
+1 22
+-1 1
+1 18
+Output
+9
+16
+16
+6
+6
+16
+15
+0
+11
+11
+9
+6
+8
+
+Limits
+Time: 3s
+Memory: 256 MB
+
+
+Solution:
+
+/*
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡖⠁⠀⠀⠀⠀⠀⠀⠈⢲⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣸⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⢀⣀⣤⣤⣤⣤⣀⡀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣔⢿⡿⠟⠛⠛⠻⢿⡿⣢⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣀⣤⣶⣾⣿⣿⣿⣷⣤⣀⡀⢀⣀⣤⣾⣿⣿⣿⣷⣶⣤⡀⠀⠀⠀⠀
+⠀⠀⢠⣾⣿⡿⠿⠿⠿⣿⣿⣿⣿⡿⠏⠻⢿⣿⣿⣿⣿⠿⠿⠿⢿⣿⣷⡀⠀⠀
+⠀⢠⡿⠋⠁⠀⠀⢸⣿⡇⠉⠻⣿⠇⠀⠀⠸⣿⡿⠋⢰⣿⡇⠀⠀⠈⠙⢿⡄⠀
+⠀⡿⠁⠀⠀⠀⠀⠘⣿⣷⡀⠀⠰⣿⣶⣶⣿⡎⠀⢀⣾⣿⠇⠀⠀⠀⠀⠈⢿⠀
+⠀⡇⠀⠀⠀⠀⠀⠀⠹⣿⣷⣄⠀⣿⣿⣿⣿⠀⣠⣾⣿⠏⠀⠀⠀⠀⠀⠀⢸⠀
+⠀⠁⠀⠀⠀⠀⠀⠀⠀⠈⠻⢿⢇⣿⣿⣿⣿⡸⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠈⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠐⢤⣀⣀⢀⣀⣠⣴⣿⣿⠿⠋⠙⠿⣿⣿⣦⣄⣀⠀⠀⣀⡠⠂⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠉⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠋⠁⠀⠀⠀
+              pshivesh8⠀⠀
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define pii pair<int,int>
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+ll gcd(ll a, ll b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(ll a, ll b, ll*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); ll x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+ll mminv(ll a, ll b) {ll arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+bool revsort(ll a, ll b) {return a > b;}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+ll combination(ll n, ll r, ll m, ll *fact, ll *ifact) {ll val1 = fact[n]; ll val2 = ifact[n - r]; ll val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<ll> sieve(int n) {int*arr = new int[n + 1](); vector<ll> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+ll phin(ll n) {ll number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (ll i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+int main(){
+    fastio();
+
+    ll n;
+    cin>>n;
+    vector<ll> a(n);
+    for(auto &it:a) cin>>it;
+
+    vector<ll> prefix(n);
+    prefix[0] = a[0];
+    for(ll i=1; i<n; i++) prefix[i] = prefix[i-1] + a[i];
+    ll t, cur = 0;
+    cin>>t;
+    while(t--){
+        ll a, b;
+        cin>>a>>b;
+        if(a == 1){
+            ll newpos=(cur+b)%n;
+            cout<<min(abs(prefix[newpos]-prefix[cur]), abs(prefix[n-1]-abs(prefix[newpos]-prefix[cur])))<<endl;
+            cur=newpos;
+        }
+        else
+        {
+            ll newpos=(((cur-b))%n +n)%n;
+            cout<<min(abs(prefix[newpos]-prefix[cur]), abs(prefix[n-1]-abs(prefix[newpos]-prefix[cur])))<<endl;
+            cur=newpos;
+        }
+
+    }
+    return 0;
+}
